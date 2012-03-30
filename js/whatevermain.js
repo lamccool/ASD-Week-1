fabe88b6298b781d88b8e960bb4c7ae5d5a6b6d4
Laura McCool <mccool_laura@yahoo.com>
2012-03-30 01:14:01 -0500

    Got some help from Shae and now I know what I'm actually doing. Amazing!

---
+++ js/whatevermain.js
@@ -0,0 +1,102 @@
+//Save Data
+
+$('#submit').live('click', function saveData(id) {
+var comments = $("#comments").val();
+var quantity = $("#quantity"). val();
+var gift-cat = $("#gift-cat").val();
+var locations = $("#locations"). val();
+var storename = $("#storename").val();
+var url = $("#url").val();
+var date = $("#date").val();
+
+var item = [comments, quantity, gift-cat, locations, storename, url, date];
+    localStorage.setItem(key, item);
+    location.reload();
+    alert("Item Saved!");
+});
+
+ function toggleControls(n) {
+    switch (n) {
+    case "on":
+        $('#giftForm').css('display', 'none');
+        $('#clear').css('display', 'inline');
+        break;
+    case "off":
+        $('#giftForm').css('display', 'block');
+        $('#clear').css('display', 'inline');
+        $('#list').css('display', 'none');
+        break;
+    default:
+        return false;
+    }
+}
+
+//Clear Data
+
+function clearLocal() {
+    if (localStorage.length === 0) {
+        alert("There is no data to clear.");
+    } else {
+        localStorage.clear();
+        alert("All data has been cleared.");
+        window.location.reload();
+        return false;
+    }
+}
+
+$("#giftForm").validate({
+    submitHandler: function(form) {
+        console.log("Call Action");
+    }
+  });
+}
+
+// Edit data
+
+/*function editItem(id) {
+    var itemId = id;
+	var value = localStorage.getItem(itemId);
+	value = value.split(',');
+	toggleControls("off");
+    var comments = value[0];
+    var quantity = value[1];
+    var gift-cat = value[2];
+    var locations = value[3];
+    var storename = value[4];
+	var url = value[5];
+	var date = value[6];
+
+
+	$("#comments").val(comments);
+	$("#quantity"). val(quantity);
+	$("#gift-cat").val(gift-cat);
+	$("#locations"). val(locations);
+	$("#storename").val(storename);
+	$("#url").val(url);
+	$("#date").val(date);
+	
+	var editButton = $('#edit-item-button').css('display', 'block');
+    var subresButtons = $('#submit-reset-buttons').css('display', 'none');
+    var itemList = $('#list').css('display', 'none');
+    
+    $('#edit-item').live('click', function clickEdit() {
+    var comments = $("#comments").val();
+	var quantity = $("#quantity"). val();
+	var gift-cat = $("#gift-cat").val();
+	var locations = $("#locations"). val();
+	var storename = $("#storename").val();
+	var url = $("#url").val();
+	var date = $("#date").val();
+
+	var item = [comments, quantity, gift-cat, locations, storename, url, date];
+	
+	localStorage.setItem(itemId, item);           
+    location.reload();
+    alert("Your List Edited!");
+    });
+
+}*/
+
+//Delete an Item
+
+
