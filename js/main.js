5e2e2f884fe29ebac57b97bde0d80b4479f6dcc6
Laura McCool <mccool_laura@yahoo.com>
2012-03-29 22:01:07 -0500

    Struggling with what to do. I'm not sure if this is right at all But now I'm working at the Clear Data function

--- js/main.js
+++ js/main.js
@@ -9,7 +9,44 @@ var storename = $("#storename").val();
 var url = $("#url").val();
 var date = $("#date").val();
 
+var item = [
+    comments, quantity, gift-cat, location, storename, url, date];
+    localStorage.setItem(key, item);
+    location.reload();
+    alert("Item Saved!");
+});
 
-}
+ function toggleControls(n) {
+    switch (n) {
+    case "on":
+        $('#giftForm').css('display', 'none');
+        $('#clear').css('display', 'inline');
+        break;
+    case "off":
+        $('#giftForm').css('display', 'block');
+        $('#clear').css('display', 'inline');
+        $('#list').css('display', 'none');
+        break;
+    default:
+        return false;
+    }
+};
 
 //Clear Data
+
+function clearLocal() {
+    if (localStorage.length === 0) {
+        alert("There is no data to clear.");
+    } else {
+        localStorage.clear();
+        alert("All data has been cleared.");
+        window.location.reload();
+        return false;
+    }
+}
+
+$("#giftForm").validate({
+    submitHandler: function(form) {
+        console.log("Call Action");
+    }
+});
